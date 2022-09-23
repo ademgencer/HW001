603aae7515fd12319e5d3bc29aa687c43996f113
ademgencer <ademyasargencer@gmail.com>
2022-09-23 08:16:37 +0300

    hw1bitti

--- src/FirstHw.java
+++ src/FirstHw.java
@@ -2,6 +2,6 @@ public class FirstHw {
     public static void main(String[] args) {
         //TODO: Lütfen ekrana Merhaba Dünya! yazan bir java programı yazınız.
         System.out.println("Merhaba Dünya 123");
-
+        System.out.println("Hello World!");
     }
 }
